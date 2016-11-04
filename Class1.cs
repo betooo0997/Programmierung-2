dbf18c1d19df9f969a7caddfe92cb5a9e724500a
Alberto L <alberto1llv@gmail.com>
2016-11-04 22:28:26 +0100

    Beispielcode für den Tilemap
    (Pseudocode, bzw. spritebatch, spritebatch.begin() usw. nicht dabei)

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Flappy_Lina
+{
+    class Class1
+    {
+        List<Texture2D> tiles;
+        int[,] tileMap;
+
+        int tileWidth;
+        int tileHeight;
+
+
+
+
+        void ContentLoader()
+        {
+            tiles = new List<Texture2D>();
+            tiles.Add(Content.Load<Texture2D>("0"));
+            tiles.Add(Content.Load<Texture2D>("1"));
+            tiles.Add(Content.Load<Texture2D>("2"));
+            tiles.Add(Content.Load<Texture2D>("3"));
+        }
+
+
+        void Initialize()
+        {
+            tileMap = new int[,]
+            {
+             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+        }
+
+
+        void Draw()
+        {
+            for (int y = 0; y < tileMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < tileMap.GetLength(1); x++)
+                {
+                    spriteBatch.Draw(tiles[tileMap[y, x]],new Vector2(x * tileWidth, y * tileHeight), Color.White);
+                }
+            }
+        }
+    }
+}
