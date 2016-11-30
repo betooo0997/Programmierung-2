4821f292a49166ae4d95362c68b9b03ad57ee530
SiegEi <pontschik@yahoo.de>
2016-11-30 23:02:26 +0100

    Begonnen mit Tile Klasse und EntityArray Klasse

---
+++ SFML Test/SFML Test/EntityArrayCreation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class EntityArrayCreation
+    {
+        protected int numberColumns;
+        protected int numberRows;
+
+        protected Entity[,] entityArray;
+
+
+        public Entity[,] GetEntityArray()
+        {
+            return entityArray;
+        }
+        
+        public int GetNumberColumns()
+        {
+            return numberColumns;
+        }
+
+        public int GetNumberRows()
+        {
+            return numberRows;
+        }
+
+        public EntityArrayCreation(Tilez[,] currentlevel, string[,] entityLayout, int columns, int rows)
+        {
+            numberColumns = columns;
+            numberRows = rows;
+
+            entityArray = new Entity[numberColumns, numberRows];
+
+
+        }
+
+    }
+}
