01eecdb09a95202684a1fe44daa05d5f822651f0
Alexander Becker <pontschik@yahoo.de>
2016-12-13 18:39:18 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	SFML Test/.vs/SFML Test/v14/.suo
    #	SFML Test/SFML Test/bin/Debug/SFML Test.exe
    #	SFML Test/SFML Test/bin/Debug/SFML Test.pdb
    #	SFML Test/SFML Test/obj/Debug/SFML Test.csproj.FileListAbsolute.txt
    #	SFML Test/SFML Test/obj/Debug/SFML Test.exe
    #	SFML Test/SFML Test/obj/Debug/SFML Test.pdb

--- SFML Test/SFML Test/Content/ContentLoader.cs
+++ SFML Test/SFML Test/Content/ContentLoader.cs
@@ -13,11 +13,14 @@ namespace Game
     class ContentLoader
     {
         public static Font fontArial;
+
         public static Texture textureDopsball;
         public static Texture textureTileSheet;
         public static Texture textureTriangle;
 
-
+        public static SoundBuffer soundClick;
+        public static SoundBuffer soundProjectileShot;
+        public static SoundBuffer soundProjectileHit;
 
         public static void LoadContent()
         {
@@ -28,6 +31,14 @@ namespace Game
             textureDopsball = new Texture("Content/Dopsball.png");
             textureTileSheet = new Texture("Content/TileSheet.png");
             textureTriangle = new Texture("Content/Triangle.png");
+
+            // SOUNDS 
+            soundClick = new SoundBuffer("Content/113087__edgardedition__click2.wav");
+            // Source: https://www.freesound.org/people/EdgardEdition/sounds/113087/
+            soundProjectileShot = new SoundBuffer("Content/156895__halgrimm__a-shot.wav");
+            // Source: https://www.freesound.org/people/Halgrimm/sounds/156895/
+            soundProjectileHit = new SoundBuffer("Content/151713__bowlingballout__pvc-rocket-cannon.wav");
+            // Source: https://www.freesound.org/people/bowlingballout/sounds/151713/
         }
     }
 }
---
+++ SFML Test/SFML Test/Managers/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using SFML.Audio;
+
+namespace Game
+{
+    static class SoundManager
+    {
+        static Sound[] soundArray;
+        static string[] soundNames;
+
+        public static void SetSoundList(Sound[] sounds, string[] names)
+        {
+            soundArray = sounds;
+            soundNames = names;
+        }
+
+        public static void PlaySpecificSound(string name)
+        {
+            for(int x = 0; x < soundNames.Length && x < soundArray.Length; x++)
+            {
+                if(soundNames[x] == name)
+                {
+                    soundArray[x].Play();
+                }
+            }
+
+        }
+    }
+}
